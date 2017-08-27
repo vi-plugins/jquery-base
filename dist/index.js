f55ed74c60227022b8ec21d1679075d0ef22dd53
Jan Rembold <jan.rembold@virtual-identity.com>
2017-08-27 11:15:06 +0200

    fixed tsconfig - see https://github.com/Microsoft/TypeScript/issues/12358

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,23 @@
-///<reference types="jquery"/>
 "use strict";
-var __extends = (this && this.__extends) || function (d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
-    function __() { this.constructor = d; }
-    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-};
-var JQueryModuleBase = (function () {
+///<reference types="jquery"/>
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+var JQueryModuleBase = /** @class */ (function () {
     function JQueryModuleBase() {
     }
     return JQueryModuleBase;
 }());
 exports.JQueryModuleBase = JQueryModuleBase;
-var JQueryPluginBase = (function (_super) {
+var JQueryPluginBase = /** @class */ (function (_super) {
     __extends(JQueryPluginBase, _super);
     /**
      * JQueryPluginBase constructor
